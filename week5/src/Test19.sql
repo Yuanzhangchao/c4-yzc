cc62bcc6c15ea5999e64f715fb557276d8b1417d
Yuan ZC <3030468836@qq.com>
2021-03-09 21:51:15 +0800

    初入数据库~~

---
+++ week5/src/Test19.sql
@@ -0,0 +1,5 @@
+create table Student(
+                        SNO varchar(20),
+                        Name varchar(10),
+                        Age Integer,
+                        College Varchar(30));
