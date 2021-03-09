eaf1ce91efe4e83aeaf69cd94dc7fab665efd9c0
Yuan ZC <3030468836@qq.com>
2021-03-09 17:12:05 +0800

    初入数据库~~

---
+++ week5/src/Test13.sql
@@ -0,0 +1,9 @@
+INSERT INTO test_temp(Name,Age,score,address)
+VALUES
+('郑七',21,490.20,'重邮宿舍11-2-1');
+INSERT INTO test_temp(Name,Age,score,address)
+VALUES
+('张八',20,560.00,'南福苑3-3-3');
+INSERT INTO test_temp(Name,Age,score,address)
+VALUES
+('王九',10,515.00,'学生新区19-7-1');
