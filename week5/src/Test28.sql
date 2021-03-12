3f7c24cdac9428353f3233fa413324356e24bc38
Yuan ZC <3030468836@qq.com>
2021-03-12 14:59:29 +0800

    初入数据库~~

---
+++ week5/src/Test28.sql
@@ -0,0 +1,4 @@
+select max(score) from choose WHERE courseID = 'C1';
+select min(score) from choose where CourseID = 'C1';
+select sum(score) from choose where courseID = 'C1';
+select avg(score) from choose where courseID = 'C1';
