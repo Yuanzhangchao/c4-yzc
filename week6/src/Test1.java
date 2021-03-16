62046da65b3c0a95cf3b3236c104533b55bfb3bc
Yuan ZC <3030468836@qq.com>
2021-03-16 12:43:35 +0800

    jdbc

---
+++ week6/src/Test1.java
@@ -0,0 +1,40 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Test1 {
+    public static void main(String[] args) {
+        Connection conn = null;
+        Statement stmt = null;
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            conn = DriverManager.getConnection("jdbc:mysql:///test", "root", "nuanlexiatian");
+            String sql = "create table student_1(SNO varchar(20),Name varchar(10),Age integer,College varchar(30))";
+            stmt = conn.createStatement();
+            int i = stmt.executeUpdate(sql);
+            System.out.println(i);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }finally {
+            if(stmt != null){
+                try {
+                    stmt.close();
+                } catch (SQLException throwables) {
+                    throwables.printStackTrace();
+                }
+            }
+            if(conn != null){
+                try {
+                    conn.close();
+                } catch (SQLException throwables) {
+                    throwables.printStackTrace();
+                }
+            }
+        }
+    }
+}
+
+ 
