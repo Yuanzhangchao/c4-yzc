f881a988db164f549c74f0ef7ab3935492ad9268
Yuanzhangchao <3030468836@qq.com>
2021-04-25 11:41:59 +0800

    反向链表

---
+++ week11_testjava/src/Test1.java
@@ -0,0 +1,37 @@
+public class Test1 {
+
+    public static void main(String[] args) {
+
+        listNode l1 = new listNode(1);
+        listNode l2 = new listNode(2);
+        listNode l3 = new listNode(3);
+        listNode l4 = new listNode(4);
+        listNode l5 = new listNode(5);
+        l1.next = l2;
+        l2.next = l3;
+        l3.next = l4;
+        l4.next= l5;
+        listNode head = l1;
+        listNode prev = null;
+        while(head != null){
+            listNode mext = head.next;
+            head.next = prev;
+            prev = head;
+            head = mext;
+        }
+        while(prev!= null){
+            System.out.println(prev.val);
+            prev = prev.next;
+        }
+    }
+}
+class listNode{
+    int val;
+    listNode next;
+    public listNode(){
+
+    }
+    public listNode(int val){
+        this.val= val;
+    }
+}
