2ec500d62e74ec46b6964c6c3b5e078c332733d2
Yuanzhangchao <3030468836@qq.com>
2021-05-07 13:45:02 +0800

    Test3

---
+++ week12_java/src/Test3.java
@@ -0,0 +1,25 @@
+public class Test3 {
+    public class ListNode {
+        int val;
+        Test1.ListNode next;
+        ListNode(int x) { val = x; }
+    }
+    public class Solution {
+        public boolean hasCycle(ListNode head) {
+            if (head == null || head.next == null) {
+                return false;
+            }
+            ListNode slow = head;
+            ListNode fast = head.next;
+            while (slow != fast) {
+                if (fast == null || fast.next == null) {
+                    return false;
+                }
+                slow = slow.next;
+                fast = fast.next.next;
+            }
+            return true;
+        }
+    }
+
+}
