9ac57ac8b50391d0dc83263fd9970d0eb6036e7d
Yuanzhangchao <3030468836@qq.com>
2021-05-07 13:44:16 +0800

    Test2

---
+++ week12_java/src/Test2.java
@@ -0,0 +1,22 @@
+public class Test2 {
+    public class ListNode {
+        int val;
+        Test1.ListNode next;
+        ListNode(int x) { val = x; }
+    }
+    class Solution {
+        public ListNode deleteDuplicates(ListNode head) {
+            ListNode cur = head;
+            while(cur != null && cur.next != null) {
+                if(cur.val == cur.next.val) {
+                    cur.next = cur.next.next;
+                } else {
+                    cur = cur.next;
+                }
+            }
+            return head;
+        }
+
+    }
+}
+
